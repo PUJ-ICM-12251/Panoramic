1ae1c687063cf04b447a15101ff8c134edcb6334
Camelo21 <martinchenhe@gmail.com>
2023-03-05 14:32:13 -0500

    Primera Entrega v2.0

--- Entrega_1/app/src/main/java/com/example/panoramic/Filtrar.java
+++ Entrega_1/app/src/main/java/com/example/panoramic/Filtrar.java
@@ -2,13 +2,30 @@ package com.example.panoramic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Filtrar extends AppCompatActivity {
 
+    Button aplicar;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtrar);
+
+        aplicar = findViewById(R.id.botonAp);
+
+        aplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Filtrar.this, BuscarEstable.class);
+                startActivity(intent);
+            }
+        });
     }
 }
